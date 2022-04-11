f1a6ea2d96944adb97c32f7798f32e8fa99304ad
matisilbert <msilbert@udesa.edu.ar>
2022-04-11 18:21:26 -0300

    commit de partials y logo etc

--- miProyecto/db/productsData.js
+++ miProyecto/db/productsData.js
@@ -19,6 +19,49 @@ let products = [
         marca:"Ford",
         modelo:"Ecosport",
         year:2021
+        
     },
+    {id:1,
+        img:"images/products/producto1.jpg",
+        name: "Ford Ecosport",
+        marca:"Ford",
+        modelo:"Ecosport",
+        year:2021
+        
+    },
+    {id:1,
+        img:"images/products/producto1.jpg",
+        name: "Ford Ecosport",
+        marca:"Ford",
+        modelo:"Ecosport",
+        year:2021
+        
+    },
+    {id:1,
+        img:"images/products/producto1.jpg",
+        name: "Ford Ecosport",
+        marca:"Ford",
+        modelo:"Ecosport",
+        year:2021
+        
+    },
+    {id:1,
+        img:"images/products/producto1.jpg",
+        name: "Ford Ecosport",
+        marca:"Ford",
+        modelo:"Ecosport",
+        year:2021
+        
+    },
+    {id:1,
+        img:"images/products/producto1.jpg",
+        name: "Ford Ecosport",
+        marca:"Ford",
+        modelo:"Ecosport",
+        descripcion: "El Porsche Cayenne S equipa un motor V6 de 2.9 litros de cilindrada con doble turbo y 324 kW (440 cv) de potencia máxima, y un par máximo de 550 Nm. El paso de 0 a 100 km/h se produce en 5,2 segundos (4,9 s. con Performance Start) alcanzando una velocidad máxima de 265 km/h.",
+        year:2021
+        
+    },
+    
 ]
 module.exports = products

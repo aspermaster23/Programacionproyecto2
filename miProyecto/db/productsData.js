1181484bcf1174fc0d6370977c6e891d5add29b5
matisilbert <msilbert@udesa.edu.ar>
2022-04-12 13:24:53 -0300

    commit de todo

--- miProyecto/db/productsData.js
+++ miProyecto/db/productsData.js
@@ -1,62 +1,69 @@
 let products = [
     {id:1,
-        img:"images/products/img-cafetera-moulinex.jpg",
-        name: "Ford Ecosport",
-        marca:"Ford",
-        modelo:"Ecosport",
-        year:2021
-    },
-    {id:1,
         img:"images/products/producto1.jpg",
-        name: "Ford Ecosport",
-        marca:"Ford",
+        name: "Porsche Cayanne",
+        marca:"Porsche",
         modelo:"Ecosport",
+        descripcion: "El Porsche Cayenne S equipa un motor V6 de 2.9 litros de cilindrada con doble turbo y 324 kW (440 cv) de potencia máxima, y un par máximo de 550 Nm. El paso de 0 a 100 km/h se produce en 5,2 segundos (4,9 s. con Performance Start) alcanzando una velocidad máxima de 265 km/h.",
         year:2021
-    },
-    {id:1,
+        
+    },{id:1,
         img:"images/products/producto1.jpg",
-        name: "Ford Ecosport",
-        marca:"Ford",
+        name: "Porsche Cayanne",
+        marca:"Porsche",
         modelo:"Ecosport",
+        descripcion: "El Porsche Cayenne S equipa un motor V6 de 2.9 litros de cilindrada con doble turbo y 324 kW (440 cv) de potencia máxima, y un par máximo de 550 Nm. El paso de 0 a 100 km/h se produce en 5,2 segundos (4,9 s. con Performance Start) alcanzando una velocidad máxima de 265 km/h.",
         year:2021
         
     },
     {id:1,
         img:"images/products/producto1.jpg",
-        name: "Ford Ecosport",
-        marca:"Ford",
+        name: "Porsche Cayanne",
+        marca:"Porsche",
         modelo:"Ecosport",
+        descripcion: "El Porsche Cayenne S equipa un motor V6 de 2.9 litros de cilindrada con doble turbo y 324 kW (440 cv) de potencia máxima, y un par máximo de 550 Nm. El paso de 0 a 100 km/h se produce en 5,2 segundos (4,9 s. con Performance Start) alcanzando una velocidad máxima de 265 km/h.",
+        year:2021
+        
+    },{id:1,
+        img:"images/products/producto1.jpg",
+        name: "Porsche Cayanne",
+        marca:"Porsche",
+        modelo:"Ecosport",
+        descripcion: "El Porsche Cayenne S equipa un motor V6 de 2.9 litros de cilindrada con doble turbo y 324 kW (440 cv) de potencia máxima, y un par máximo de 550 Nm. El paso de 0 a 100 km/h se produce en 5,2 segundos (4,9 s. con Performance Start) alcanzando una velocidad máxima de 265 km/h.",
         year:2021
         
     },
     {id:1,
         img:"images/products/producto1.jpg",
-        name: "Ford Ecosport",
-        marca:"Ford",
+        name: "Porsche Cayanne",
+        marca:"Porsche",
         modelo:"Ecosport",
+        descripcion: "El Porsche Cayenne S equipa un motor V6 de 2.9 litros de cilindrada con doble turbo y 324 kW (440 cv) de potencia máxima, y un par máximo de 550 Nm. El paso de 0 a 100 km/h se produce en 5,2 segundos (4,9 s. con Performance Start) alcanzando una velocidad máxima de 265 km/h.",
         year:2021
         
     },
     {id:1,
         img:"images/products/producto1.jpg",
-        name: "Ford Ecosport",
-        marca:"Ford",
+        name: "Porsche Cayanne",
+        marca:"Porsche",
         modelo:"Ecosport",
+        descripcion: "El Porsche Cayenne S equipa un motor V6 de 2.9 litros de cilindrada con doble turbo y 324 kW (440 cv) de potencia máxima, y un par máximo de 550 Nm. El paso de 0 a 100 km/h se produce en 5,2 segundos (4,9 s. con Performance Start) alcanzando una velocidad máxima de 265 km/h.",
         year:2021
         
     },
     {id:1,
         img:"images/products/producto1.jpg",
-        name: "Ford Ecosport",
-        marca:"Ford",
+        name: "Porsche Cayanne",
+        marca:"Porsche",
         modelo:"Ecosport",
+        descripcion: "El Porsche Cayenne S equipa un motor V6 de 2.9 litros de cilindrada con doble turbo y 324 kW (440 cv) de potencia máxima, y un par máximo de 550 Nm. El paso de 0 a 100 km/h se produce en 5,2 segundos (4,9 s. con Performance Start) alcanzando una velocidad máxima de 265 km/h.",
         year:2021
         
     },
     {id:1,
         img:"images/products/producto1.jpg",
-        name: "Ford Ecosport",
-        marca:"Ford",
+        name: "Porsche Cayanne",
+        marca:"Porsche",
         modelo:"Ecosport",
         descripcion: "El Porsche Cayenne S equipa un motor V6 de 2.9 litros de cilindrada con doble turbo y 324 kW (440 cv) de potencia máxima, y un par máximo de 550 Nm. El paso de 0 a 100 km/h se produce en 5,2 segundos (4,9 s. con Performance Start) alcanzando una velocidad máxima de 265 km/h.",
         year:2021

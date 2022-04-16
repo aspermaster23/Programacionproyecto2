b522d3cdbc8e76c36fe32f54f259736cfae962f5
Emilia-Tarascio-udesa <etarascio@udesa.edu.ar>
2022-04-16 15:29:18 -0300

    Ultimas modificaciones

--- miProyecto/db/productsData.js
+++ miProyecto/db/productsData.js
@@ -1,34 +1,34 @@
 let products = [
-    {
-       "id":1,
-       "img":"/images/products/Porsche.png",
-       "name":"Porsche Cayenne",
-       "marca":"Porsche",
-       "modelo":"Ecosport",
-       "descripcion":"El Porsche Cayenne S equipa un motor V6 de 2.9 litros de cilindrada con doble turbo y 324 kW (440 cv) de potencia máxima, y un par máximo de 550 Nm. El paso de 0 a 100 km/h se produce en 5,2 segundos (4,9 s. con Performance Start) alcanzando una velocidad máxima de 265 km/h.",
-       "year":2021,
-       "comments":[
-         {
-            "name":"Pedro",
-            "lastname":"Ruiz",
-            "img":"https://img.freepik.com/foto-gratis/apuesto-hombre-empresario-sonriendo-alegre_176420-17877.jpg?w=2000",
-            "texto":"Auto lindo!"
-         },
-         {
-            "name":"Julian",
-            "lastname":"Gonzalez",
-            "img":"https://img.freepik.com/foto-gratis/retrato-estudio-hombre-moreno-confianza_1187-5799.jpg?w=2000",
-            "texto":"Que buen motor!"
-         },
-         {
-            "name":"Ernesto ",
-            "lastname":"Guzman",
-            "img":"https://conceptodefinicion.de/wp-content/uploads/2015/03/hombre.jpg",
-            "texto":"¿Es grande el baul?"
-         }
-      ]
-    },
-    {
+   {
+      "id":1,
+      "img":"https://phantom-marca.unidadeditorial.es/8564fa122f23ce2ade03fa498bb000a7/resize/1320/f/jpg/assets/multimedia/imagenes/2021/06/30/16250401118863.jpg",
+      "name":"Porsche Cayenne",
+      "marca":"Porsche",
+      "modelo":"Ecosport",
+      "descripcion":"El Porsche Cayenne S equipa un motor V6 de 2.9 litros de cilindrada con doble turbo y 324 kW (440 cv) de potencia máxima, y un par máximo de 550 Nm. El paso de 0 a 100 km/h se produce en 5,2 segundos (4,9 s. con Performance Start) alcanzando una velocidad máxima de 265 km/h.",
+      "year":2021,
+      "comments":[
+      {
+         "name":"Pedro",
+         "lastname":"Ruiz",
+         "img":"https://img.freepik.com/foto-gratis/apuesto-hombre-empresario-sonriendo-alegre_176420-17877.jpg?w=2000",
+         "texto":"Auto lindo!"
+      },
+      {
+         "name":"Julian",
+         "lastname":"Gonzalez",
+         "img":"https://img.freepik.com/foto-gratis/retrato-estudio-hombre-moreno-confianza_1187-5799.jpg?w=2000",
+         "texto":"Que buen motor!"
+      },
+      {
+         "name":"Ernesto ",
+         "lastname":"Guzman",
+         "img":"https://conceptodefinicion.de/wp-content/uploads/2015/03/hombre.jpg",
+         "texto":"¿Es grande el baul?"
+      }
+   ]
+   },
+   {
       "id":1,
       "img":"https://cdn.motor1.com/images/mgl/XpwWv/s1/2020-mercedes-amg-e-63-limousine.jpg",
       "name":"Mercedes AMG E63",
@@ -57,181 +57,181 @@ let products = [
          }
       ]
    },
-    {
-       "id":1,
-       "img":"https://www.km77.com/images/medium/6/6/5/7/900.326657.jpg",
-       "name":"BMW M2 Coupé",
-       "marca":"BMW",
-       "modelo":"M2 Coupé",
-       "descripcion":"El BMW M2 Coupé 2018 tiene un motor naftero de 2979cc con 6 cilindros ubicados en línea que alcanza una potencia máxima de 370CV a 6500rpm y par máximo de 500Nm a 1400-5560rpm. Esta mecánica permite recorrer el 0 a 100 km/h en 4.5 segundos y alcanzar una velocidad máxima de 250 km/h (limitada).",
-       "year":2018,
-       "comments":[
-          {
-             "name":"Pilar",
-             "lastname":"Garcia",
-             "img":"https://www.unwomen.org/sites/default/files/Communications/Headquarters/Images/02_WhatWeDo_LeadershipAndPoliticalParticipation_675x350.jpg?la=en",
-             "texto":"Ideal para la carretera"
+   {
+      "id":1,
+      "img":"https://www.km77.com/images/medium/6/6/5/7/900.326657.jpg",
+      "name":"BMW M2 Coupé",
+      "marca":"BMW",
+      "modelo":"M2 Coupé",
+      "descripcion":"El BMW M2 Coupé 2018 tiene un motor naftero de 2979cc con 6 cilindros ubicados en línea que alcanza una potencia máxima de 370CV a 6500rpm y par máximo de 500Nm a 1400-5560rpm. Esta mecánica permite recorrer el 0 a 100 km/h en 4.5 segundos y alcanzar una velocidad máxima de 250 km/h (limitada).",
+      "year":2018,
+      "comments":[
+         {
+            "name":"Pilar",
+            "lastname":"Garcia",
+            "img":"https://www.unwomen.org/sites/default/files/Communications/Headquarters/Images/02_WhatWeDo_LeadershipAndPoliticalParticipation_675x350.jpg?la=en",
+            "texto":"Ideal para la carretera"
          },
-          {
-             "name":"Julian",
-             "lastname":"Gonzalez",
-             "img":"https://img.freepik.com/foto-gratis/retrato-estudio-hombre-moreno-confianza_1187-5799.jpg?w=2000",
-             "texto":"Muy lindo!"
-          },
-          {
-             "name":"Ernesto ",
-             "lastname":"Guzman",
-             "img":"https://conceptodefinicion.de/wp-content/uploads/2015/03/hombre.jpg",
-             "texto":"Que tal las llantas?"
-          }
-       ]
-    },
-    {
-       "id":1,
-       "img":"https://cdn.motor1.com/images/mgl/LQgOR/s1/lanzamiento-ford-f-150-raptor.jpg",
-       "name":"Ford F-150 Raptor",
-       "marca":"Ford",
-       "modelo":"F-150 Raptor",
-       "descripcion":"Con un motor EcoBoost Bi-Turbo V6 3.5L de alto rendimiento acompañado con una transmisión automática de 10 velocidades, la nueva F‑150 Raptor puede hacer frente a cualquier terreno. Gracias a sus 455CV de potencia y 691nm de torque, esta pick-up acelera de 0 a 100 km/h en 7.2 segundos alcanzando una velocidad máxima de 175 km/h.",
-       "year":2020,
-       "comments":[
-          {
-             "name":"Juliana",
-             "lastname":"Mendoza",
-             "img":"https://fotografias.antena3.com/clipping/cmsimages01/2021/05/02/26E03450-C5FB-4D16-BC9B-B282AE784352/57.jpg",
-             "texto":"Me encanta el color"
-          },
-          { 
-             "name":"Milagros",
-             "lastname":"Juarez",
-             "img":"https://static01.nyt.com/images/2017/05/07/arts/07GAL-GADOTweb/07GAL-GADOTweb-articleLarge.jpg?quality=75&auto=webp&disable=upscale",
-             "texto":"AMAMOS"
-          },
-          {
-             "name":"Pedro",
-             "lastname":"Acosta",
-             "img":"https://www.cdc.gov/preconception/spanish/images/man-with-arms-crossed.jpg",
-             "texto":"Muchas ganas de probarla"
-          },
-       ]
-    },
-    {
-       "id":1,
-       "img":"https://cdn.motor1.com/images/mgl/eXLLA/s1/2020-audi-sq7-tdi.jpg",
-       "name":"Audi SQ7",
-       "marca":"Audi",
-       "modelo":"SQ7",
-       "descripcion":"El Audi SQ7 equipa un motor 4.0 TFSI que le permite acelerar de 0 a 100 km/h en 4,1 segundos y su velocidad máxima está limitada a 250 km/h.",
-       "year":2020,
-       "comments":[
-          {
-             "name":"Juliana",
-             "lastname":"Mendoza",
-             "img":"https://fotografias.antena3.com/clipping/cmsimages01/2021/05/02/26E03450-C5FB-4D16-BC9B-B282AE784352/57.jpg",
-             "texto":"Me encanta!"
-          },
-          {
-             "name":"Rodrigo",
-             "lastname":"Perez",
-             "img":"https://static2.abc.es/media/summum/2021/10/01/maxi_iglesias-kXKH--620x349@abc.jpeg",
-             "texto":"Quiero unooo"
-          },
-          {
-             "name":"Milagros",
-             "lastname":"Juarez",
-             "img":"https://static01.nyt.com/images/2017/05/07/arts/07GAL-GADOTweb/07GAL-GADOTweb-articleLarge.jpg?quality=75&auto=webp&disable=upscale",
-             "texto":"Este quiero para mi cumpleaños"
-          }
-       ]
-    },
-    {
-       "id":1,
-       "img":"https://acnews.blob.core.windows.net/imgnews/paragraph/NPAZ_f0a841a66af1495a9b0d3ab612747b66.jpg",
-       "name":"Volkswagen Golf R",
-       "marca":"Volkswagen",
-       "modelo":"Golf R",
-       "descripcion":"El Golf R, un vehículo altamente deportivo que acelera de 0 a 100 km/h entre 4.7 segundos y alcanza una velocidad máxima de 250 km/h gracias a su motor 2.0 turbo. Dejando de lado su deportividad, tranquilamente puede ser un auto de diario, ya que es sumamente cómodo y suave.",
-       "year":2022,
-       "comments":[
-          {
-             "name":"Juliana",
-             "lastname":"Mendoza",
-             "img":"https://fotografias.antena3.com/clipping/cmsimages01/2021/05/02/26E03450-C5FB-4D16-BC9B-B282AE784352/57.jpg",
-             "texto":"Me encanta!"
-          },
-          {
-             "name":"Rodrigo",
-             "lastname":"Perez",
-             "img":"https://static2.abc.es/media/summum/2021/10/01/maxi_iglesias-kXKH--620x349@abc.jpeg",
-             "texto":"Quiero unooo"
-          },
-          {
-             "name":"Milagros",
-             "lastname":"Juarez",
-             "img":"https://static01.nyt.com/images/2017/05/07/arts/07GAL-GADOTweb/07GAL-GADOTweb-articleLarge.jpg?quality=75&auto=webp&disable=upscale",
-             "texto":"Este quiero para mi cumpleaños"
-          }
-       ]
-    },
-    {
-       "id":1,
-       "img":"https://periodismodelmotor.com/wp-content/uploads/2019/12/toyota-gr-yaris-2020-1.jpg",
-       "name":"Toyota Yaris GR",
-       "marca":"Toyota",
-       "modelo":"Yaris GR",
-       "descripcion":"El motor de 3 cilindros del GR Yaris produce 272 cv y 370 Nm, la manera de entregar su potencia al asfalto es mediante tracción integral denominada GR-Four. El conjunto mecánico que presenta este vehículo hace posible una aceleración de 0 a 100 km/h en 5.5 segundos y una velocidad máxima de 230 km/h.",
-       "year":2020,
-       "comments":[
-          {
-             "name":"Juliana",
-             "lastname":"Mendoza",
-             "img":"https://fotografias.antena3.com/clipping/cmsimages01/2021/05/02/26E03450-C5FB-4D16-BC9B-B282AE784352/57.jpg",
-             "texto":"Me encanta!"
-          },
-          {
-             "name":"Rodrigo",
-             "lastname":"Perez",
-             "img":"https://static2.abc.es/media/summum/2021/10/01/maxi_iglesias-kXKH--620x349@abc.jpeg",
-             "texto":"Quiero unooo"
-          },
-          {
-             "name":"Milagros",
-             "lastname":"Juarez",
-             "img":"https://static01.nyt.com/images/2017/05/07/arts/07GAL-GADOTweb/07GAL-GADOTweb-articleLarge.jpg?quality=75&auto=webp&disable=upscale",
-             "texto":"Este quiero para mi cumpleaños"
-          }
-       ]
-    },
-    {
-       "id":1,
-       "img":"https://www.blogdecoches.net/wp-content/uploads/2017/06/Salón-del-Automóvil-en-Buenos-Aires-01.jpg",
-       "name":"Chevrolet Camaro",
-       "marca":"Chevrolet",
-       "modelo":"Camaro",
-       "descripcion":"La sexta generación del Camaro, es la más fuerte hasta el momento gracias a su motor 6.2L V8, 461 CV de potencia y 617 Nm de torque. Este modelo cuenta con una capacidad de aceleración de 0 a 100 Km/h, en 4,2 segundos. Su velocidad máxima es de 250 km/h autolimitada.",
-       "year":2017,
-       "comments":[
-          {
-             "name":"Juliana",
-             "lastname":"Mendoza",
-             "img":"https://fotografias.antena3.com/clipping/cmsimages01/2021/05/02/26E03450-C5FB-4D16-BC9B-B282AE784352/57.jpg",
-             "texto":"Me encanta!"
-          },
-          {
-             "name":"Rodrigo",
-             "lastname":"Perez",
-             "img":"https://static2.abc.es/media/summum/2021/10/01/maxi_iglesias-kXKH--620x349@abc.jpeg",
-             "texto":"Quiero unooo"
-          },
-          {
-             "name":"Milagros",
-             "lastname":"Juarez",
-             "img":"https://static01.nyt.com/images/2017/05/07/arts/07GAL-GADOTweb/07GAL-GADOTweb-articleLarge.jpg?quality=75&auto=webp&disable=upscale",
-             "texto":"Este quiero para mi cumpleaños"
-          }
-       ]
-    },
-    {
+         {
+            "name":"Julian",
+            "lastname":"Gonzalez",
+            "img":"https://img.freepik.com/foto-gratis/retrato-estudio-hombre-moreno-confianza_1187-5799.jpg?w=2000",
+            "texto":"Muy lindo!"
+         },
+         {
+            "name":"Ernesto ",
+            "lastname":"Guzman",
+            "img":"https://conceptodefinicion.de/wp-content/uploads/2015/03/hombre.jpg",
+            "texto":"Que tal las llantas?"
+         }
+      ]
+   },
+   {
+      "id":1,
+      "img":"https://cdn.motor1.com/images/mgl/LQgOR/s1/lanzamiento-ford-f-150-raptor.jpg",
+      "name":"Ford F-150 Raptor",
+      "marca":"Ford",
+      "modelo":"F-150 Raptor",
+      "descripcion":"Con un motor EcoBoost Bi-Turbo V6 3.5L de alto rendimiento acompañado con una transmisión automática de 10 velocidades, la nueva F‑150 Raptor puede hacer frente a cualquier terreno. Gracias a sus 455CV de potencia y 691nm de torque, esta pick-up acelera de 0 a 100 km/h en 7.2 segundos alcanzando una velocidad máxima de 175 km/h.",
+      "year":2020,
+      "comments":[
+         {
+            "name":"Juliana",
+            "lastname":"Mendoza",
+            "img":"https://fotografias.antena3.com/clipping/cmsimages01/2021/05/02/26E03450-C5FB-4D16-BC9B-B282AE784352/57.jpg",
+            "texto":"Me encanta el color"
+         },
+         { 
+            "name":"Milagros",
+            "lastname":"Juarez",
+            "img":"https://static01.nyt.com/images/2017/05/07/arts/07GAL-GADOTweb/07GAL-GADOTweb-articleLarge.jpg?quality=75&auto=webp&disable=upscale",
+            "texto":"AMAMOS"
+         },
+         {
+            "name":"Pedro",
+            "lastname":"Acosta",
+            "img":"https://www.cdc.gov/preconception/spanish/images/man-with-arms-crossed.jpg",
+            "texto":"Muchas ganas de probarla"
+         },
+      ]
+   },
+   {
+      "id":1,
+      "img":"https://cdn.motor1.com/images/mgl/eXLLA/s1/2020-audi-sq7-tdi.jpg",
+      "name":"Audi SQ7",
+      "marca":"Audi",
+      "modelo":"SQ7",
+      "descripcion":"El Audi SQ7 equipa un motor 4.0 TFSI que le permite acelerar de 0 a 100 km/h en 4,1 segundos y su velocidad máxima está limitada a 250 km/h.",
+      "year":2020,
+      "comments":[
+         {
+            "name":"Juliana",
+            "lastname":"Mendoza",
+            "img":"https://fotografias.antena3.com/clipping/cmsimages01/2021/05/02/26E03450-C5FB-4D16-BC9B-B282AE784352/57.jpg",
+            "texto":"Me encanta!"
+         },
+         {
+            "name":"Rodrigo",
+            "lastname":"Perez",
+            "img":"https://static2.abc.es/media/summum/2021/10/01/maxi_iglesias-kXKH--620x349@abc.jpeg",
+            "texto":"Quiero unooo"
+         },
+         {
+            "name":"Milagros",
+            "lastname":"Juarez",
+            "img":"https://static01.nyt.com/images/2017/05/07/arts/07GAL-GADOTweb/07GAL-GADOTweb-articleLarge.jpg?quality=75&auto=webp&disable=upscale",
+            "texto":"Este quiero para mi cumpleaños"
+         }
+      ]
+   },
+   {
+      "id":1,
+      "img":"https://acnews.blob.core.windows.net/imgnews/paragraph/NPAZ_f0a841a66af1495a9b0d3ab612747b66.jpg",
+      "name":"Volkswagen Golf R",
+      "marca":"Volkswagen",
+      "modelo":"Golf R",
+      "descripcion":"El Golf R, un vehículo altamente deportivo que acelera de 0 a 100 km/h entre 4.7 segundos y alcanza una velocidad máxima de 250 km/h gracias a su motor 2.0 turbo. Dejando de lado su deportividad, tranquilamente puede ser un auto de diario, ya que es sumamente cómodo y suave.",
+      "year":2022,
+      "comments":[
+         {
+            "name":"Juliana",
+            "lastname":"Mendoza",
+            "img":"https://fotografias.antena3.com/clipping/cmsimages01/2021/05/02/26E03450-C5FB-4D16-BC9B-B282AE784352/57.jpg",
+            "texto":"Me encanta!"
+         },
+         {
+            "name":"Rodrigo",
+            "lastname":"Perez",
+            "img":"https://static2.abc.es/media/summum/2021/10/01/maxi_iglesias-kXKH--620x349@abc.jpeg",
+            "texto":"Quiero unooo"
+         },
+         {
+            "name":"Milagros",
+            "lastname":"Juarez",
+            "img":"https://static01.nyt.com/images/2017/05/07/arts/07GAL-GADOTweb/07GAL-GADOTweb-articleLarge.jpg?quality=75&auto=webp&disable=upscale",
+            "texto":"Este quiero para mi cumpleaños"
+         }
+      ]
+   },
+   {
+      "id":1,
+      "img":"https://periodismodelmotor.com/wp-content/uploads/2019/12/toyota-gr-yaris-2020-1.jpg",
+      "name":"Toyota Yaris GR",
+      "marca":"Toyota",
+      "modelo":"Yaris GR",
+      "descripcion":"El motor de 3 cilindros del GR Yaris produce 272 cv y 370 Nm, la manera de entregar su potencia al asfalto es mediante tracción integral denominada GR-Four. El conjunto mecánico que presenta este vehículo hace posible una aceleración de 0 a 100 km/h en 5.5 segundos y una velocidad máxima de 230 km/h.",
+      "year":2020,
+      "comments":[
+         {
+            "name":"Juliana",
+            "lastname":"Mendoza",
+            "img":"https://fotografias.antena3.com/clipping/cmsimages01/2021/05/02/26E03450-C5FB-4D16-BC9B-B282AE784352/57.jpg",
+            "texto":"Me encanta!"
+         },
+         {
+            "name":"Rodrigo",
+            "lastname":"Perez",
+            "img":"https://static2.abc.es/media/summum/2021/10/01/maxi_iglesias-kXKH--620x349@abc.jpeg",
+            "texto":"Quiero unooo"
+         },
+         {
+            "name":"Milagros",
+            "lastname":"Juarez",
+            "img":"https://static01.nyt.com/images/2017/05/07/arts/07GAL-GADOTweb/07GAL-GADOTweb-articleLarge.jpg?quality=75&auto=webp&disable=upscale",
+            "texto":"Este quiero para mi cumpleaños"
+         }
+      ]
+   },
+   {
+      "id":1,
+      "img":"https://www.blogdecoches.net/wp-content/uploads/2017/06/Salón-del-Automóvil-en-Buenos-Aires-01.jpg",
+      "name":"Chevrolet Camaro",
+      "marca":"Chevrolet",
+      "modelo":"Camaro",
+      "descripcion":"La sexta generación del Camaro, es la más fuerte hasta el momento gracias a su motor 6.2L V8, 461 CV de potencia y 617 Nm de torque. Este modelo cuenta con una capacidad de aceleración de 0 a 100 Km/h, en 4,2 segundos. Su velocidad máxima es de 250 km/h autolimitada.",
+      "year":2017,
+      "comments":[
+         {
+            "name":"Juliana",
+            "lastname":"Mendoza",
+            "img":"https://fotografias.antena3.com/clipping/cmsimages01/2021/05/02/26E03450-C5FB-4D16-BC9B-B282AE784352/57.jpg",
+            "texto":"Me encanta!"
+         },
+         {
+            "name":"Rodrigo",
+            "lastname":"Perez",
+            "img":"https://static2.abc.es/media/summum/2021/10/01/maxi_iglesias-kXKH--620x349@abc.jpeg",
+            "texto":"Quiero unooo"
+         },
+         {
+            "name":"Milagros",
+            "lastname":"Juarez",
+            "img":"https://static01.nyt.com/images/2017/05/07/arts/07GAL-GADOTweb/07GAL-GADOTweb-articleLarge.jpg?quality=75&auto=webp&disable=upscale",
+            "texto":"Este quiero para mi cumpleaños"
+         }
+      ]
+   },
+   {
       "id":1,
       "img":"https://www.purosautos.com/wp-content/uploads/2021/11/DG022_132CLeepllg6c6vqvlt96trm9sr4ikk.jpg",
       "name":"Dodge Challenger SRT Hellcat Redeye",
@@ -347,8 +347,8 @@ let products = [
          }
       ]
    }
-    
- ]
+
+]
 
 
 module.exports = products

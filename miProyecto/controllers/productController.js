02ab148d620c1ce617b28a09f132d7229c668f10
matisilbert <msilbert@udesa.edu.ar>
2022-04-17 17:28:29 -0300

    errores

--- miProyecto/controllers/productController.js
+++ miProyecto/controllers/productController.js
@@ -1,9 +1,9 @@
-let producto = require("../db/productsData")
+let productos = require("../db/productsData")
 let user = require("../db/usersData")
 let productController = {
     detail: (req, res)=> {
       let indice = req.params.indice
-        res.render('product', { title: 'Detalle de Producto' ,product: producto[indice]});
+        res.render('product', { title: 'Detalle de Producto' ,product: productos[indice]});
       },
       add: (req, res)=> {
         res.render('product-add', { title: 'Agregar producto', user:user });

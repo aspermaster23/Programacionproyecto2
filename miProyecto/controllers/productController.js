af6d53a73df30f994def86ae3749a4e4a41958da
matisilbert <msilbert@udesa.edu.ar>
2022-06-02 17:32:48 -0300

    detalle producto

--- miProyecto/controllers/productController.js
+++ miProyecto/controllers/productController.js
@@ -1,12 +1,34 @@
 let productos = require("../db/productsData")
 let user = require("../db/usersData")
+let db = require("../database/models")
+
 let productController = {
-    detail: (req, res)=> {
-      let indice = req.params.indice
-        res.render('product', { title: 'Detalle de Producto' ,product: productos[indice]});
-      },
-      add: (req, res)=> {
-        res.render('product-add', { title: 'Agregar producto', user:user });
-      }
+  detail: (req, res) => {
+    let indice = req.params.indice
+    db.Car.findByPk(indice, {
+      include: [{
+          association: 'comment',
+          include: {
+            association: 'user'
+          }
+        },
+        {
+          association: 'user'
+        }
+      ]
+    }).then(producto => {
+      // res.send(producto)
+      res.render('product', {
+        title: 'Detalle de Producto',
+        product: producto
+      });
+    })
+  },
+  add: (req, res) => {
+    res.render('product-add', {
+      title: 'Agregar producto',
+      user: user
+    });
+  }
 }
 module.exports = productController

fa755ddf0db550c504641b3cd9bd07152db482a0
matisilbert <msilbert@udesa.edu.ar>
2022-06-06 17:15:35 -0300

    base de datos
    correccion de la base de datos y agregue datos y los relacione con el visual.

--- miProyecto/db/digitalcars_db.sql
+++ miProyecto/db/digitalcars_db.sql
@@ -43,11 +43,15 @@ FOREIGN KEY (car_id) REFERENCES cars(id)
 INSERT INTO users 
 VALUES
 (DEFAULT, "matuteCABJ", "matiRaviol@gmail.com", "flanmixto2",  "1994-04-12", "matute.png", DEFAULT, DEFAULT);
+(DEFAULT, "gonchiruiz", "gonzalito@gmail.com", "eltucu12",  "1992-03-17", "gonchi.png", DEFAULT, DEFAULT,
+(DEFAULT, "asper23", "asper@gmail.com", "aspermaster",  "1991-06-22", "asper.png", DEFAULT, DEFAULT),  
+(DEFAULT, "petsafras", "pit@gmail.com", "vigilante2",  "1995-10-07", "pete.png", DEFAULT, DEFAULT);
 
 INSERT INTO cars
 VALUES
 (DEFAULT, 1, "Porsche", "Silver","Cayenne", "El Porsche Cayenne S equipa un motor V6 de 2.9 litros de cilindrada con doble turbo y 324 kW (440 cv) de potencia máxima, y un par máximo de 550 Nm. El paso de 0 a 100 km/h se produce en 5,2 segundos (4,9 s. con Performance Start) alcanzando una velocidad máxima de 265 km/h.", 2021, "Porsche.png", DEFAULT, DEFAULT);
-
+(default, 2, "BMW","white","M2 Coupe","El BMW M2 Coupé 2018 tiene un motor naftero de 2979cc con 6 cilindros ubicados en línea que alcanza una potencia máxima de 370CV a 6500rpm y par máximo de 500Nm a 1400-5560rpm. Esta mecánica permite recorrer el 0 a 100 km/h en 4.5 segundos y alcanzar una velocidad máxima de 250 km/h (limitada).","Bmw.jpg", DEFAULT, DEFAULT),
+(DEFAULT, 3, "Mercedes", "Silver","E63", "Motor V8 4.0 bi-turbonaftero (612 cv y 850 Nm), combinado con caja automática AMG Speedshift MCT 9G (la primera con embragues múltiples del mundo) y tracción integral 4Matic+. LO MÁS: Las prestaciones. Acelera de 0 a 100 km/h en 3.4 segundos y alcanza una velocidad máxima de 300 km/h.", 2021, "mercedes.jpg", DEFAULT, DEFAULT);
 INSERT INTO comments
 VALUES
 (DEFAULT, 1, 1, "Auto lindo!", DEFAULT, DEFAULT ),

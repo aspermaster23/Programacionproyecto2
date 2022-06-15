20c54973732ae0faa69765cd92de5456e3425655
matisilbert <msilbert@udesa.edu.ar>
2022-06-15 17:02:13 -0300

    Crear producto

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,6 +3,12 @@ let user = require("../db/usersData")
 let db = require("../database/models")
 
 let productController = {
+  add: (req, res) => {
+    res.render('product-add', {
+      title: 'Agregar producto',
+   
+    });
+  },
   detail: (req, res) => {
     let indice = req.params.indice
     db.Car.findByPk(indice, {
@@ -24,11 +30,29 @@ let productController = {
       });
     })
   },
-  add: (req, res) => {
-    res.render('product-add', {
-      title: 'Agregar producto',
-      user: user
-    });
+  
+  storeCar:(req,res)=>{
+    
+  
+  
+    let car ={
+      user_id: req.body.user_id,
+      car_brand: req.body.car_brand,
+      car_color: req.body.car_color,
+      car_type: req.body.car_type,
+      car_description: req.body.car_description,
+      car_year: req.body.car_year,
+      car_image:req.file.filename,
+      created_at:Date.now(),
+    }
+  
+    db.Car.create(car)
+    .then(info =>{
+      return res.redirect('/')
+    }).catch(e=>{
+      console.log(e)
+    })
   }
+ 
 }
 module.exports = productController
--- routes/products.js
+++ routes/products.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 const productController = require('../controllers/productController');
 
+let multer = require('multer');
+let path = require('path');
+
+//Configurar multer.
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, '../public/images/products/')) //Usamos path.join para evitar problemas de rutas. __dirname da la posición exacta de la carpeta en la que está el archivo. Luego desde ahí nos movemos hasta la carpeta public.
+    //Las carpetas deben existir.
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+  }
+})
+ 
+var upload = multer({ storage: storage })
 /* GET home page. */
-router.get('/:indice', productController.detail);
 router.get('/add', productController.add);
+router.post('/add',upload.single("car_image"), productController.storeCar);
+router.get('/:indice', productController.detail);
 module.exports = router;
